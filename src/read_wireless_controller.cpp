b49b81312f358b87cae4aead0a84a53df33a48bd
SJTU-MKH <1761374236@qq.com>
2024-07-02 15:46:38 +0800

    add pub_controller

--- src/read_wireless_controller.cpp
+++ src/read_wireless_controller.cpp
@@ -3,6 +3,7 @@
  **/
 #include "rclcpp/rclcpp.hpp"
 #include "unitree_go/msg//wireless_controller.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 using std::placeholders::_1;
 
@@ -11,6 +12,7 @@ class wireless_controller_suber : public rclcpp::Node
 public:
   wireless_controller_suber() : Node("wireless_controller_suber")
   {
+    pub_controller = this->create_publisher<std_msgs::msg::Float64MultiArray>("/unitree_go/controller_array", 20);
     // the cmd_puber is set to subscribe "/wirelesscontroller" topic
     suber = this->create_subscription<unitree_go::msg::WirelessController>(
         "/wirelesscontroller", 10, std::bind(&wireless_controller_suber::topic_callback, this, _1));
@@ -27,9 +29,13 @@ private:
 
     RCLCPP_INFO(this->get_logger(), "Wireless controller -- lx: %f; ly: %f; rx: %f; ry: %f; key value: %d",
                 data->lx, data->ly, data->rx, data->ry, data->keys);
+    auto message = std_msgs::msg::Float64MultiArray();
+    message.data = {data->lx, data->ly, data->rx, data->ry};
+    pub_controller->publish(message);
   }
 
   rclcpp::Subscription<unitree_go::msg::WirelessController>::SharedPtr suber;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_controller;
 };
 
 int main(int argc, char *argv[])
